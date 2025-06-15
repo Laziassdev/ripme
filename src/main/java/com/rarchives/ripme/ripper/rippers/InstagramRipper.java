6931856538ed7ca70dd1c61c01bc5afc42882c21
Laziassdev <tinglaziass@gmail.com>
2025-06-15 18:10:23 +0100

    Update InstagramRipper.java

--- src/main/java/com/rarchives/ripme/ripper/rippers/InstagramRipper.java
+++ src/main/java/com/rarchives/ripme/ripper/rippers/InstagramRipper.java
@@ -27,6 +27,7 @@ public class InstagramRipper extends AbstractJSONRipper {
     private Map<String, String> cookies = new HashMap<>();
     private String endCursor = null;
     private boolean hasNextPage = true;
+    private boolean fallbackToGraphQL = false;
 
     public InstagramRipper(URL url) throws IOException {
         super(url);
@@ -58,7 +59,10 @@ public class InstagramRipper extends AbstractJSONRipper {
         Document document = Http.url(url).cookies(cookies).response().parse();
         JSONObject jsonObject = getJsonObjectFromDoc(document);
         if (jsonObject == null) {
-            throw new IOException("Unable to parse Instagram page JSON. Possible invalid session or private profile.");
+            logger.warn("Failed to parse sharedData, falling back to GraphQL");
+            fallbackToGraphQL = true;
+            String username = getGID(url);
+            return getGraphQLUserPage(username, null);
         }
         JSONObject user = getJsonObjectByPath(jsonObject, "entry_data.ProfilePage[0].graphql.user");
         if (user.optBoolean("is_private") && user.optJSONObject("edge_owner_to_timeline_media") == null) {
@@ -73,18 +77,30 @@ public class InstagramRipper extends AbstractJSONRipper {
 
     @Override
     public JSONObject getNextPage(JSONObject json) throws IOException {
-        if (!hasNextPage || endCursor == null || idString == null) {
+        if (!hasNextPage || idString == null) {
             return null;
+        }
+        String username = getGID(url);
+        return getGraphQLUserPage(username, endCursor);
+    }
+
+    private JSONObject getGraphQLUserPage(String username, String afterCursor) throws IOException {
+        if (idString == null) {
+            // Lookup user ID first
+            Document doc = Http.url("https://www.instagram.com/" + username + "/?__a=1").cookies(cookies).get();
+            JSONObject shared = new JSONObject(doc.body().text());
+            idString = shared.getJSONObject("graphql").getJSONObject("user").getString("id");
         }
         JSONObject variables = new JSONObject();
         variables.put("id", idString);
         variables.put("first", 12);
-        variables.put("after", endCursor);
+        if (afterCursor != null) {
+            variables.put("after", afterCursor);
+        }
+        String queryHash = "c6809c9c025875ac6f02619eae97a80e";
+        String fullUrl = format("https://www.instagram.com/graphql/query/?query_hash=%s&variables=%s", queryHash, variables.toString());
 
-        String queryHash = "c6809c9c025875ac6f02619eae97a80e"; // standard query hash for profile posts
-        String url = format("https://www.instagram.com/graphql/query/?query_hash=%s&variables=%s", queryHash, variables.toString());
-
-        JSONObject result = Http.url(url).cookies(cookies).getJSON();
+        JSONObject result = Http.url(fullUrl).cookies(cookies).getJSON();
         JSONObject media = result.getJSONObject("data").getJSONObject("user").getJSONObject("edge_owner_to_timeline_media");
         JSONObject pageInfo = media.getJSONObject("page_info");
         hasNextPage = pageInfo.getBoolean("has_next_page");
@@ -134,7 +150,12 @@ public class InstagramRipper extends AbstractJSONRipper {
     public List<String> getURLsFromJSON(JSONObject json) {
         List<String> result = new ArrayList<>();
         try {
-            JSONArray edges = getJsonObjectByPath(json, "entry_data.ProfilePage[0].graphql.user.edge_owner_to_timeline_media.edges").getJSONArray("edges");
+            JSONArray edges;
+            if (fallbackToGraphQL) {
+                edges = json.getJSONObject("data").getJSONObject("user").getJSONObject("edge_owner_to_timeline_media").getJSONArray("edges");
+            } else {
+                edges = getJsonObjectByPath(json, "entry_data.ProfilePage[0].graphql.user.edge_owner_to_timeline_media.edges").getJSONArray("edges");
+            }
             for (int i = 0; i < edges.length(); i++) {
                 JSONObject node = edges.getJSONObject(i).getJSONObject("node");
                 if (node.optBoolean("is_video", false)) {
@@ -145,22 +166,8 @@ public class InstagramRipper extends AbstractJSONRipper {
                     result.add(node.getString("display_url"));
                 }
             }
-        } catch (Exception e) {
-            try {
-                JSONArray edges = json.getJSONObject("data").getJSONObject("user").getJSONObject("edge_owner_to_timeline_media").getJSONArray("edges");
-                for (int i = 0; i < edges.length(); i++) {
-                    JSONObject node = edges.getJSONObject(i).getJSONObject("node");
-                    if (node.optBoolean("is_video", false)) {
-                        if (node.has("video_url")) {
-                            result.add(node.getString("video_url"));
-                        }
-                    } else {
-                        result.add(node.getString("display_url"));
-                    }
-                }
-            } catch (Exception ex) {
-                logger.warn("Failed to extract media URLs", ex);
-            }
+        } catch (Exception ex) {
+            logger.warn("Failed to extract media URLs", ex);
         }
         return result;
     }
